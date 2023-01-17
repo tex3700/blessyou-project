6ba0ea6dd7510eb66ec87a130c60bf980bd2ac31
Dmitrii <shokilla@mail.ru>
2023-01-17 13:17:11 +0400

    added code in headerContent

--- front/src/component/header/topBar.jsx
+++ front/src/component/header/topBar.jsx
@@ -1,6 +1,7 @@
 import {
   AppBar,
   Box,
+  Button,
   Container,
   Grid,
   Toolbar,
@@ -10,27 +11,27 @@ import React from "react";
 import MailIcon from "@mui/icons-material/Mail";
 import PhoneIcon from "@mui/icons-material/Phone";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
+import useStyles from "./styles";
 
 const TopBar = () => {
   return (
-    <Box sx={{ flexGrow: 1 }} className="topBarBox">
-      <AppBar>
+    <AppBar>
+      <Container fixed>
         <Toolbar>
-          <Container fixed>
-            <Grid container>
-              <PhoneIcon />
-
-              <Typography>8 800 777 77 77</Typography>
-
-              <MailIcon />
-              <Typography>info@blessyou.ru</Typography>
-              <LocationOnIcon />
-              <Typography>г.Москва, Ивановская площадь</Typography>
-            </Grid>
-          </Container>
+          <PhoneIcon />
+          <Typography>8 800 777 77 77</Typography>
+          <MailIcon />
+          <Typography>info@blessyou.ru</Typography>
+          <LocationOnIcon />
+          <Typography>г.Москва, Ивановская площадь</Typography>
+          <Box>
+            <Button color="inherit" variant="outlined">
+              Log In
+            </Button>
+          </Box>
         </Toolbar>
-      </AppBar>
-    </Box>
+      </Container>
+    </AppBar>
   );
 };
 
