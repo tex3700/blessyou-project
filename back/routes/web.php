f398479edde4cabc4e8d79bc39a781d09db0fea5
Roman <romaymniy@gmail.com>
2023-01-19 14:19:50 +0300

    feat:  Развернул копию  проекта с git + Попробовал сделать комминт

--- back/routes/web.php
+++ back/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return "HelloWorld";
 });
 
 require __DIR__.'/auth.php';
