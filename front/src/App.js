9d0940416987e97f5053c1006b2e2799d915da2d
Vin.S <gift121279@gmail.com>
2023-01-23 13:21:39 +0300

    1

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import "./App.scss";
 import { Route, Link, Routes } from "react-router-dom";
-import { Receipts, Appointment } from "./component";
+import { Receipts, Appointment, Services } from "./component";
 import Header from "./component/header/header";
 import { Container } from "@material-ui/core";
 import Main from "./component/mainPage/main";
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/about" element={<Appointment />} />
-          <Route path="/services" element={<Appointment />} />
+          <Route path="/services" element={<Services />} />
           <Route path="/doctors" element={<Appointment />} />
           <Route path="/contacts" element={<Receipts />} />
           <Route path="/patientAccout" element={<Appointment />} />
