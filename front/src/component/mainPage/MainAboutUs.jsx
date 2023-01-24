766687ac3e534d7fdd6887b54def1e5a8f475c1a
Dmitrii <shokilla@mail.ru>
2023-01-24 15:59:46 +0400

    decomposition main + created MainAboutUs

---
+++ front/src/component/mainPage/MainAboutUs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import useStyles from "./styles";
+import { Box, Button, Container, Grid, Typography } from "@material-ui/core";
+
+/////
+
+const MainAboutUs = () => {
+  const classes = useStyles();
+
+  return <Container fixed>Ghbdtn</Container>;
+};
+
+export default MainAboutUs;
--- front/src/component/mainPage/styles.js
+++ front/src/component/mainPage/styles.js
@@ -1,20 +1,19 @@
 import { makeStyles } from "@material-ui/core";
-import image from "./../../static/image/mainPageBunner.jpg";
+import mainPageBunner from "./../../static/image/mainPageBunner.jpg";
+import mainAboutUs from "./../../static/image/mainAboutUs.jpg";
 
 const useStyles = makeStyles((theme) => ({
+  ////////////////////////
+  ////styles for MainPaper
   mainPaper: {
     position: "relative",
     color: theme.palette.common.black,
-    // marginBottom: theme.spacing(5),
-    backgroundImage: `url(${image})`,
+    backgroundImage: `url(${mainPageBunner})`,
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center",
     height: "750px",
     borderRadius: "0",
-
-    // borderColor:
-    //   " linear-gradient(90.48deg, rgba(22, 47, 98, 0.8) -1.21%, rgba(125, 214, 246, 0) 75.47%)",
   },
 
   mainBox: {
@@ -29,7 +28,6 @@ const useStyles = makeStyles((theme) => ({
 
   mainTextParagraf: {
     width: "597px",
-    // fontWeight: "500",
     fontSize: "32px",
     lineHeight: "40px",
   },
@@ -54,6 +52,9 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     textTransform: "uppercase",
   },
+
+  ////////////////////////////////
+  ////styles for MainWelcome
 }));
 
 export default useStyles;
