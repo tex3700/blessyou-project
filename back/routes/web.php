f818fcdeee06ee3bc77e82b9a71c5979d0e2befc
Roman <romaymniy@gmail.com>
2023-01-26 10:49:44 +0300

    feat: Добавил тестовые запросы для докторов.

--- back/routes/web.php
+++ back/routes/web.php
@@ -15,14 +15,7 @@ use App\Http\Controllers\User;
  */
 
 Route::get('/', function () {
-    return "Test 4";
+    return "";
 });
-Route::get('/greeting', function () {
-    return 'Hello World';
-});
-Route::prefix('/user')->group(function () {
-    Route::get('/', [User\UserController::class, 'index']);
-    Route::get('/get/{user}', [User\UserController::class, 'getUser']);
-    Route::get('/template/{user}', [User\UserController::class, 'template']);
-});
+
 require __DIR__ . '/auth.php';
