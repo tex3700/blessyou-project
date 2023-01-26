8922ba4a623e83ba6f100566f39ea2e7e986139d
Roman <romaymniy@gmail.com>
2023-01-26 10:46:30 +0300

    feat: Добавил тестовые запросы для докторов.

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\DoctorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::controller(DoctorController::class)->group(function () {
+    Route::get('/doctors', 'index');
+    Route::post('/doctors/update/name', 'changeName');
+});
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
