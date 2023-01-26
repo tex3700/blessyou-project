5c6b789caf14f2de4a811df67562928bad1fea7a
tex3700 <tex3700@gmail.com>
2023-01-26 23:11:43 +0300

    Modified controller EmployeeController цÑ with User-Employee relation

--- back/app/Http/Controllers/API/EmployeeController.php
+++ back/app/Http/Controllers/API/EmployeeController.php
@@ -3,29 +3,44 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Models\Employee;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Employee;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Builder;
 
 class EmployeeController extends Controller
 {
+
+    private Builder $model;
+
+    public function __construct()
+    {
+        $this->model = User::query()
+            ->join('employees', 'employees.user_id', '=', 'users.id');
+    }
+
     public function index(): JsonResponse
     {
-        $employees = Employee::all();
+        $employees = $this->model->get();
 
         return response()->json($employees);
     }
 
     public function store(Request $request): JsonResponse
     {
+        $user = User::create($request->all());
         $employee = Employee::create($request->all());
 
-        return response()->json( $employee, 201);
+        $user->employee()->save($employee);
+
+        return response()->json( 'saved', 201 );
     }
 
     public function edit($id): JsonResponse
     {
-        $employee = Employee::all()->find($id);
+        $employee = $this->model->get()->find($id);
 
         return response()->json($employee);
     }
@@ -33,13 +48,16 @@ class EmployeeController extends Controller
     public function update(Request $request, Employee $employee): JsonResponse
     {
         $employee->update($request->all());
+        $employee->user()->update($request->all());
 
-        return response()->json($employee);
+        return response()->json('updated');
     }
 
     public function destroy(Employee $employee): JsonResponse
     {
         $employee->delete();
-        return response()->json(null, 204);
+        $employee->user()->delete();
+
+        return response()->json('deleted', 204);
     }
 }
