872436329beff365f2e91510e1f89fea9fa65bad
Adil <adil_gummetov@mail.ru>
2023-02-01 12:39:28 +0300

    image copy

--- front/src/component/DoctorList/doctorlList.jsx
+++ front/src/component/DoctorList/doctorlList.jsx
@@ -9,6 +9,10 @@ const doctorCardList = [
   { text: "Петрова А.А.",  title: "Педиатр",         image: "https://wmpics.space/di-1JYP.jpg", path: "/doctorlist" },
   { text: "Смирнов А.Б.",  title: "Гастроэнтеролог", image: "https://wmpics.space/di-RR9F.jpg", path: "/doctorlist" },
   { text: "Сидорова Н.В.", title: "Отоларинголог",   image: "https://wmpics.space/di-M72A.jpg", path: "/doctorlist" },
+  { text: "Иванов И.И.",   title: "Физеотерапевт",   image: "https://wmpics.space/di-PNZ3.jpg", path: "/doctorlist" },
+  { text: "Петрова А.А.",  title: "Педиатр",         image: "https://wmpics.space/di-1JYP.jpg", path: "/doctorlist" },
+  { text: "Смирнов А.Б.",  title: "Гастроэнтеролог", image: "https://wmpics.space/di-RR9F.jpg", path: "/doctorlist" },
+  { text: "Сидорова Н.В.", title: "Отоларинголог",   image: "https://wmpics.space/di-M72A.jpg", path: "/doctorlist" },
 ];
 
 const DoctorList = () => {
