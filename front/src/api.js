1cf78b11d77cab9b3d13481e73b7e761327b6b78
balueva <81beu@mail.ru>
2023-02-02 15:51:49 +0700

    buil 0451

--- front/src/api.js
+++ front/src/api.js
@@ -1,9 +1,11 @@
 
 const apiUrl = process.env.NODE_ENV !== 'production' ? 'https://blessyou-clinic.ru' : '';
-
 export const apiRequest = (endpoint, method, data) => {
     const s = `${apiUrl}/api/api/${endpoint}`;
+    console.log('process.env.NODE_ENV = ', process.env.NODE_ENV);
+    console.log('apiUrl = ', apiUrl);
     console.log('s = ', s);
+
     return fetch(s, {
         method: method,
         headers: { 'Content-Type': 'application/json' },
