cc6303057ba3c7036fe532f41320498fc8a3fc56
balueva <81beu@mail.ru>
2023-02-02 15:27:16 +0700

    Корректировка apiurl в api.js

--- front/src/api.js
+++ front/src/api.js
@@ -2,7 +2,9 @@
 const apiUrl = process.env.NODE_ENV !== 'production' ? 'https://blessyou-clinic.ru' : '';
 
 export const apiRequest = (endpoint, method, data) => {
-    return fetch(`${apiUrl}/api/api/${endpoint}`, {
+    const s = `${apiUrl}/api/api/${endpoint}`;
+    console.log('s = ', s);
+    return fetch(s, {
         method: method,
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
