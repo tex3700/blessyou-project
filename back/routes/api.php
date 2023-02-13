58299a45c81f15eaace451446051f717d241b810
tex3700 <tex3700@gmail.com>
2023-02-13 21:54:26 +0300

    Correct api routes after conflict

--- back/routes/api.php
+++ back/routes/api.php
@@ -5,12 +5,8 @@ use App\Http\Controllers\API\DoctorController;
 use App\Http\Controllers\API\PatientController;
 use App\Http\Controllers\API\SpecialityController;
 use App\Http\Controllers\API\DepartmentController;
-<<<<<<< HEAD
 use App\Http\Controllers\Auth\LoginController;
-=======
-use App\Http\Controllers\API\PatientController;
 use App\Http\Controllers\API\MailController;
->>>>>>> f63cfeef631ad5fe8e9b8e541d97f9f60850a20c
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
