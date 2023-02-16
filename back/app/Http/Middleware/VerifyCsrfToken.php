67d55a873e5cb63f248f0a15720f65fac0aad834
Dmitry <35429999+tex3700@users.noreply.github.com>
2023-02-16 21:43:05 +0300

    Update VerifyCsrfToken.php
    Закомитил исключение

--- back/app/Http/Middleware/VerifyCsrfToken.php
+++ back/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //"http://https://blessyou-clinic.ru/api/*",
+        //"https://blessyou-clinic.ru/api/*",
     ];
 }
