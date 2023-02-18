683a8160e99d4020131725ec3a7766d7cedce28b
Vin.S <gift121279@gmail.com>
2023-02-18 12:48:36 +0300

    update activeAppointments

---
+++ front/src/component/activeAppointments/activeAppointments.js
@@ -0,0 +1,27 @@
+import { Box, Container, Typography } from "@material-ui/core";
+import React, { useContext } from "react";
+// import { useStyles } from "../medicalHistory/styleMedical";
+import { MedicalCard, useStyles } from "../medicalHistory/";
+import { DataContext } from "../../DataContext";
+
+export const ActiveAppointments = () => {
+  const classes = useStyles();
+  const { doctorArray } = useContext(DataContext);
+
+  // console.log("doctorArray", doctorArray);
+
+  return (
+    <>
+      <Container fixed>
+        <Box className={classes.medicalTitlleBox}>
+          <Typography className={classes.medicalTitlle}>
+            Дата и специалист
+          </Typography>
+        </Box>
+        <MedicalCard props={doctorArray[5]} />
+        <MedicalCard props={doctorArray[7]} />
+      </Container>
+      <Box style={{ minHeight: "300px" }}></Box>
+    </>
+  );
+};
